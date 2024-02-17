37c0d3ff5c03ff12d75a772700cc20c8605d8266
AngelaVilladiego <villadia@sheridancollege.ca>
2024-02-17 18:59:59 -0500

    workin

--- SovesticoAPI/app.py
+++ SovesticoAPI/app.py
@@ -10,18 +10,46 @@ from bson.objectid import ObjectId
 from pymongo import MongoClient
 import json
 import requests
+import csv
+import urllib.parse
 
 app = Flask(__name__)
 CORS(app)
 
 load_dotenv(find_dotenv())
 mongo_pwd = os.getenv('MONGO_PWD')
+mongo_pwd = urllib.parse.quote_plus('{mongo_pwd}')
 mongo_user = os.getenv('MONGO_USER')
+mongo_user = urllib.parse.quote_plus('{mongo_user}')
 
-client = MongoClient(
-    f'mongodb+srv://{mongo_user}:{mongo_pwd}@sovestico.lngt1xe.mongodb.net/?retryWrites=true&w=majority')
-db = client.main
+client = MongoClient('mongodb+srv://%s:%s@sovestico.lngt1xe.mongodb.net/?retryWrites=true&w=majority' % (mongo_user, mongo_pwd))
+db = client.sovestico
+
+stocks_collection = db.stocks
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/addBaseRecords", methods=['GET'])
+def addBaseRecords():
+    res = []    
+     
+    # Open a csv reader called DictReader
+    with open("./stock_db.csv", encoding='utf-8') as csvf:
+        csvReader = csv.DictReader(csvf)
+         
+        # Convert each row into a dictionary 
+        # and add it to data
+        for rows in csvReader:
+             
+            # Assuming a column named 'No' to
+            # be the primary key
+            data = {}
+            key = rows['symbol']
+            data["_id"] = key
+            data.update(rows)
+            res.append(data)
+ 
+    stocks_collection.insert_many(res)
+    return ""
