580940fc30354c4f91524332aa7164c720d91119
Ken Wu <wooqianghao@gmail.com>
2024-02-17 18:17:48 -0500

    Updated data

--- SovesticoAPI/stock.py
+++ SovesticoAPI/stock.py
@@ -1,41 +1,4 @@
-def get_stock_data(stock_symbol: str) -> dict:
-    """
-    Get stock data from Alpha Vantage API
-    request arguments: the symbol of the stock
-    returns: the in depth information of the stock including:
-        name and other basic stock information like data,
-        strongestPrinciple and source/reasoning for strongest principle as a short blurb,
-        weakestPrinciple and source/reasoning for weakest principle as a short blurb,
-        relevant news articles, 
-        summary of relevant news articles,
-        summary from our chatbot about why it suggests this,
-        in depth data about change and growth,
-    """
-    # API Key
-
-def get_stock_news(stock_symbol: str) -> dict:
-    """
-    Get stock news from News API
-    """
-    # API Key
-    
-def get_individual_stock_data(stock_symbol: str) -> dict:
-    """
-    Get stock data from Alpha Vantage API
-    request arguments: the symbol of the stock
-    returns: the in depth information of the stock including:
-        name and other basic stock information like data,
-        strongestPrinciple and source/reasoning for strongest principle as a short blurb,
-        weakestPrinciple and source/reasoning for weakest principle as a short blurb,
-        relevant news articles, 
-        summary of relevant news articles,
-        summary from our chatbot about why it suggests this,
-        in depth data about change and growth,
-    """
-    # API Key
-    pass
-
-def stock_rec(stock_symbol: str) -> dict:
+def stock_rec(filters: list) -> dict:
     """
     Get stock recommendation from Sovestico API
     //request arguments: a list of principles to filter by
@@ -57,5 +20,29 @@ def stock_rec(stock_symbol: str) -> dict:
             ]
     }
     """
-    # get stocks from SP500.csv
-    sp_df = pd.read_csv('SP500.csv')
+    # read from MongoDB
+    # filter by principles
+    # return paginated data
+    for f in filters:
+        if f == ""
+
+def get_stock_data(stock_symbol: str) -> dict:
+    """
+    Get stock data from Alpha Vantage API
+    request arguments: the symbol of the stock
+    returns: the in depth information of the stock including:
+        name and other basic stock information like data,
+        strongestPrinciple and source/reasoning for strongest principle as a short blurb,
+        weakestPrinciple and source/reasoning for weakest principle as a short blurb,
+        relevant news articles, 
+        summary of relevant news articles,
+        summary from our chatbot about why it suggests this,
+        in depth data about change and growth,
+    """
+    # API Key
+
+def get_stock_news(stock_symbol: str) -> dict:
+    """
+    Get stock news from News API
+    """
+    
